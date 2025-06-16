a610f89ba535d6c62a30c7f1e91c4da36bc14cca
Maxwell Chamberlain <maxintegllc@gmail.com>
2025-06-16 15:38:41 -0600

    Add placeholder on suspense boundary

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -22,11 +22,66 @@ import type z from "zod";
 
 export default function ContactPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PagePlaceholder />}>
       <Page />
     </Suspense>
   );
 }
+
+const PagePlaceholder = () => {
+  return (
+    <div className="relative pb-32">
+      <div className="z-50 mx-auto flex w-screen max-w-7xl flex-col items-center justify-center gap-8 pt-12">
+        <Badge className="fade-in-0 zoom-in-95 animate-in !bg-indigo-500/15 !text-indigo-500 duration-700">
+          We&apos;d love to hear from you!
+        </Badge>
+        <h1 className="fade-in-0 zoom-in-95 animate-in text-4xl font-bold duration-700">Contact us</h1>
+        <p className="fade-in-0 zoom-in-95 animate-in text-muted-foreground max-w-2xl text-center duration-700">
+          Want to get project specifications or pricing? Or just want to ask about one of our services? We&apos;re here
+          to get you the best solutions, the fastest.
+        </p>
+        <div className="w-full max-w-2xl" key="contact">
+          <Card className="w-full max-w-2xl">
+            <CardHeader>
+              <CardTitle>Get in touch!</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-4">
+                  <Input label="Name" placeholder="Who are you?" required type="text" autoComplete="name" disabled />
+                  <Input
+                    label="Email"
+                    placeholder="What's your email?"
+                    required
+                    type="email"
+                    autoComplete="email"
+                    disabled
+                  />
+                  <Select disabled>
+                    <SelectTrigger label="Type" className="w-full" disabled>
+                      <SelectValue placeholder="What are you looking for?" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {CONTACT_US_TYPE.map((type) => (
+                        <SelectItem key={type.key} value={type.key}>
+                          {type.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <Textarea label="Message" placeholder="What's your message?" required disabled />
+                  <Button type="submit" className="w-full !bg-indigo-500" disabled>
+                    Hold on...
+                  </Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Page = () => {
   const router = useRouter();
