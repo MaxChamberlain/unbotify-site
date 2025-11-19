05e93b552aa8456204a673928b2e5a51d4123e4d
Maxwell Chamberlain <maxintegllc@gmail.com>
2025-11-19 12:25:56 -0700

    add form honeypot

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,18 +1,13 @@
-import type * as React from 'react';
+import type * as React from "react";
 
-import { cn } from '@/lib/utils';
-import { Label } from '@radix-ui/react-label';
-import { Badge } from './badge';
+import { cn } from "@/lib/utils";
+import { Label } from "@radix-ui/react-label";
+import { Badge } from "./badge";
 
-function Input({
-  className,
-  type,
-  label,
-  ...props
-}: React.ComponentProps<'input'> & { label?: string }) {
+function Input({ className, type, label, ...props }: React.ComponentProps<"input"> & { label?: string }) {
   if (label) {
     return (
-      <div className="flex flex-col gap-1">
+      <div className={cn("flex flex-col gap-1", className)}>
         <Label htmlFor={label} className="ml-1.5 text-xs font-medium">
           {label}
         </Label>
@@ -20,10 +15,10 @@ function Input({
           type={type}
           data-slot="input"
           className={cn(
-            'border-input selection:bg-primary selection:text-primary-foreground file:text-foreground placeholder:text-muted-foreground dark:bg-input/30 flex h-8 w-full min-w-0 rounded-xl border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-            'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-            'aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40',
-            className
+            "border-input selection:bg-primary selection:text-primary-foreground file:text-foreground placeholder:text-muted-foreground dark:bg-input/30 flex h-8 w-full min-w-0 rounded-xl border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+            "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
+            className,
           )}
           {...props}
         />
@@ -35,10 +30,10 @@ function Input({
       type={type}
       data-slot="input"
       className={cn(
-        'border-input selection:bg-primary selection:text-primary-foreground file:text-foreground placeholder:text-muted-foreground dark:bg-input/30 flex h-8 w-full min-w-0 rounded-xl border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-        'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-        'aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40',
-        className
+        "border-input selection:bg-primary selection:text-primary-foreground file:text-foreground placeholder:text-muted-foreground dark:bg-input/30 flex h-8 w-full min-w-0 rounded-xl border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
+        className,
       )}
       {...props}
     />
