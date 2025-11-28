cc91cad9c67a41cfa41d894104b11eecc30aa1ab
Maxwell Chamberlain <maxintegllc@gmail.com>
2025-11-28 08:55:51 -0700

    Initial Commit

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -2,34 +2,29 @@ import { TRPCError, initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
-import { auth } from "@/server/auth";
 import { db } from "@/server/db";
 import type { PrismaClient } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 import type { Session } from "next-auth";
 
 export const createTRPCContext = async (opts: { headers: Headers }) => {
-	const session = await auth();
-
-	return {
-		db,
-		session,
-		...opts,
-	};
+  return {
+    db,
+    ...opts,
+  };
 };
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
-	transformer: superjson,
-	errorFormatter({ shape, error }) {
-		return {
-			...shape,
-			data: {
-				...shape.data,
-				zodError:
-					error.cause instanceof ZodError ? error.cause.flatten() : null,
-			},
-		};
-	},
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError: error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
 });
 
 export const createCallerFactory = t.createCallerFactory;
@@ -37,59 +32,32 @@ export const createCallerFactory = t.createCallerFactory;
 export const createTRPCRouter = t.router;
 
 const timingMiddleware = t.middleware(async ({ next, path }) => {
-	const start = Date.now();
+  const start = Date.now();
 
-	if (t._config.isDev) {
-		// artificial delay in dev
-		const waitMs = Math.floor(Math.random() * 400) + 100;
-		await new Promise((resolve) => setTimeout(resolve, waitMs));
-	}
+  if (t._config.isDev) {
+    // artificial delay in dev
+    const waitMs = Math.floor(Math.random() * 400) + 100;
+    await new Promise((resolve) => setTimeout(resolve, waitMs));
+  }
 
-	const result = await next();
+  const result = await next();
 
-	const end = Date.now();
-	console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
+  const end = Date.now();
+  console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
 
-	return result;
+  return result;
 });
 
 export const publicProcedure = t.procedure.use(timingMiddleware);
 
-export const protectedProcedure = t.procedure
-	.use(timingMiddleware)
-	.use(({ ctx, next }) => {
-		if (!ctx.session?.user) {
-			throw new TRPCError({ code: "UNAUTHORIZED" });
-		}
-		return next({
-			ctx: {
-				// infers the `session` as non-nullable
-				session: { ...ctx.session, user: ctx.session.user },
-			},
-		});
-	});
-
 export type PublicProcedureContext = {
-	headers: Headers;
-	db: PrismaClient<
-		{
-			log: ("warn" | "error")[];
-			datasourceUrl: string;
-		},
-		never,
-		DefaultArgs
-	>;
-};
-
-export type ProtectedProcedureContext = {
-	headers: Headers;
-	db: PrismaClient<
-		{
-			log: ("warn" | "error")[];
-			datasourceUrl: string;
-		},
-		never,
-		DefaultArgs
-	>;
-	session: Session;
+  headers: Headers;
+  db: PrismaClient<
+    {
+      log: ("warn" | "error")[];
+      datasourceUrl: string;
+    },
+    never,
+    DefaultArgs
+  >;
 };
