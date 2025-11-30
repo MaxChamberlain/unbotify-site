6c75b90fe0ca4c118ec040cb92e6f473e34774cf
Maxwell Chamberlain <maxintegllc@gmail.com>
2025-11-30 09:23:07 -0700

    Change to ad language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,21 +91,21 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en" className={`${geist.variable}`}>
       <head>
-        <Script>
+        <script>
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-538FWKFG');`}
-        </Script>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-17260632331"></Script>
-        <Script>
+        </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17260632331"></script>
+        <script>
           {`window.dataLayer = window.dataLayer ||[];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'AW-17260632331');`}
-        </Script>
+        </script>
       </head>
       <body>
         <Script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }} />
